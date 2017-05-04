67ea4c98d42452b20761f8687e435efe35a47d30
James Mitchell <raspberryjamberlin@gmail.com>
2017-05-04 13:02:26 +0200

    Loads of changes.
    Saving files to /tmp/ and added dump command so that a user can purge
    the /tmp/ to a picci folder

--- picci.py
+++ picci.py
@@ -1,4 +1,6 @@
 import io
+import shutil
+import os
 import picamera
 import curses
 from time import sleep
@@ -10,6 +12,9 @@ class MyOutput(object):
         self.output = None
         self.lapse = False
         self.interval = 3
+        self.movefiles = False
+        self.source = '/tmp/'
+        self.dest = '/home/pi/picci/'
 
     def write(self, buf):
         if buf.startswith(b'\xff\xd8'):
@@ -18,7 +23,7 @@ class MyOutput(object):
             self.output = None
             if self.write_frame:
                 self.file_num += 1
-                self.output = io.open('%d.jpg' % self.file_num, 'wb')
+                self.output = io.open('/tmp/%d.jpg' % self.file_num, 'wb')
                 if self.lapse == True:
                     sleep(self.interval)
                     self.write_frame = True
@@ -27,9 +32,16 @@ class MyOutput(object):
         if self.output:
             self.output.write(buf)
 
+    def movef(self):
+        if self.movefiles == True:
+            files = os.listdir(source)
+            for f in files:
+                shutil.move(f, dest)
+            self.movefiles = false
+
 def main(window):
     window.nodelay(True)
-    with picamera.PiCamera(resolution=(1280, 720), framerate=15) as camera:
+    with picamera.PiCamera(resolution=(2592, 1944), framerate=15) as camera:
         output = MyOutput()
         camera.start_recording(output, format='mjpeg')
         try:
@@ -42,6 +54,9 @@ def main(window):
                 window.addstr(6, 0, 'Use the up and down arrow keys to adjust timelapse interval')
                 window.addstr(7, 0, 'Timelapse interval in secounds is: %i     ' % output.interval)
                 window.addstr(10, 0, 'Frame %d.jpg' % output.file_num)
+                window.addstr(14, 0, 'Files saved from the timelapse or single are stored in the /tmp/ directory.')
+                window.addstr(15, 0, 'If the system reboots these files are purged.')
+                window.addstr(16, 0, 'Press D to dump image files from /tmp/ to home directory')
                 window.refresh()
                 c = window.getch()
                 if c == ord('q'):
@@ -50,22 +65,22 @@ def main(window):
                     output.write_frame = True
                 elif c == ord('3'):
                     if output.lapse == True:
-                        window.addstr(9, 0, 'Timelapse already running')
+                        window.addstr(9, 0, 'Timelapse already running                      ')
                     else:
                         output.lapse = True
                         output.interval = 3
                         output.write_frame = True
-                        window.addstr(9, 0, 'Timelapse started                ')
+                        window.addstr(9, 0, 'Timelapse started                              ')
                 elif c == ord('s'):
                     if output.lapse == True:
-                        window.addstr(9, 0, 'Timelapse already running')
+                        window.addstr(9, 0, 'Timelapse already running                      ')
                     else:
                         output.lapse = True
                         output.write_frame = True
-                        window.addstr(9, 0, 'Timelapse started                ')
+                        window.addstr(9, 0, 'Timelapse started                              ')
                 elif c == ord('p'):
                     output.lapse = False
-                    window.addstr(9, 0, 'Timelapse paused                  ')
+                    window.addstr(9, 0, 'Timelapse paused                                   ')
                 elif c == curses.KEY_UP:
                     if output.lapse == False:
                         output.interval = output.interval + 1
@@ -73,6 +88,16 @@ def main(window):
                     if output.lapse == False:
                         if output.interval > 1:
                             output.interval = output.interval - 1
+                elif c == ord('d'):
+                    if output.lapse == True:
+                        window.addstr(9, 0, 'Can not dump files while timelapse is running')
+                    else:
+                        for files in os.listdir(output.source):
+                            if files.endswith(".jpg"):
+                                source_files = os.path.join(output.source, files)
+                                dest_files = os.path.join(output.dest, files)
+                                shutil.move(source_files, dest_files)
+                                window.addstr(17, 0, 'File dump complate!')
                 camera.wait_recording(0.1)
         finally:
             camera.stop_recording()
